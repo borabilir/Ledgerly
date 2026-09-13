4fa52baa4f0de1c086e6c0a00fe0b751ee633958
Bora Bilir <bora.bilir@outlook.com>
2026-09-14 02:36:59 +0300

    feat(infra): add initial database migration

---
+++ src/Ledgerly.Infrastructure/Persistence/Migrations/20260913233429_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ledgerly.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "wallets",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    owner_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    balance = table.Column<decimal>(type: "numeric(19,4)", precision: 19, scale: 4, nullable: false),
+                    created_at_utc = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_wallets", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ux_wallets_owner_id_currency",
+                table: "wallets",
+                columns: new[] { "owner_id", "currency" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "wallets");
+        }
+    }
+}
